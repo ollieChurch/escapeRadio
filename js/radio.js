c020cc9261be90e7547113f5b0f5298d49367b8b
Squeak-Paradox <olliemchurch@gmail.com>
2021-12-08 19:42:39 +0000

    combines music data into admin js file

--- js/radio.js
+++ js/radio.js
@@ -1,3 +1,46 @@
+const musicData = [
+    {
+        name: "Alex Swift Show Start",
+        src: "../audio/1ParadoxRadio.mp3",
+    },
+    {
+        name: "Alex Swift Track 10 Intro",
+        src: "../audio/2ParadoxRadio.mp3",
+    },
+    {
+        name: "Track 10 - I Don't Recognise Myself ",
+        src: "../audio/3ParadoxRadio.mp3",
+    },
+    {
+        name: "Alex Swift Track 10 Outro",
+        src: "../audio/4ParadoxRadio.mp3",
+    },
+    {
+        name: "Alex Swift Track 9 Intro",
+        src: "../audio/5ParadoxRadio.mp3",
+    },
+    {
+        name: "Track 9 - Don't Lie To Me",
+        src: "../audio/6ParadoxRadio.mp3",
+    },
+    {
+        name: "Ad Break",
+        src: "../audio/7ParadoxRadio.mp3",
+    },
+    {
+        name: "Jack Mellow Escape Rooms",
+        src: "../audio/8ParadoxRadio.mp3",
+    },
+    {
+        name: "Alex Swift Track 8 Intro",
+        src: "../audio/9ParadoxRadio.mp3",
+    },
+    {
+        name: "Track 8 - Shadows",
+        src: "../audio/10ParadoxRadio.mp3",
+    }
+]
+
 // ===== TIME REMAINING SET UP ===== //
 
 const gameMinsDisplay = document.getElementById("timeDisplay-mins")
@@ -25,7 +68,6 @@ let selectedEndTrack
 const playlistContainer = document.getElementById("playlist-container")
 const setUpContainer = document.getElementById("setUp-container")
 
-import { musicData } from "./musicData.js"
 let playlistTrackCards = document.getElementsByClassName("playlist-trackCard")
 let currentTrack
 let trackCardHeight
